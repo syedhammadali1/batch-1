fb4c3e28f606c3a2f9f065344de444fe724cae44
syedhammadali1 <hammadali407292@gmail.com>
2023-11-27 10:18:47 -0800

    2nd class

--- javascript/first-class/script.js
+++ javascript/first-class/script.js
@@ -1,1 +1,9 @@
 alert('this code from externel file');
+// console.log('hi');  
+console.log('bye'); 
+
+let lastname, lastName;
+lastName = "Peterson";
+lastname = "Doe";
+
+console.log(lastname,'lastname');
