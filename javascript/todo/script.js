281d33a6c8435d82e96f522df742c5b3bbb611f2
syedhammadali1 <hammadali407292@gmail.com>
2023-12-31 08:47:07 -0800

    .

--- javascript/todo/script.js
+++ javascript/todo/script.js
@@ -2,6 +2,14 @@
 function addTask(){
   let value =  document.getElementById('taskInput').value;
 
-  localStorage.setItem('task1',value);
-  console.log(element);
+  if (value) {
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.push({ task: value });
+    // console.log(tasks);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    // localStorage.setItem('task1',value);  
+    document.getElementById('taskInput').value = '';
+  }
+  // console.log(element);
 }
