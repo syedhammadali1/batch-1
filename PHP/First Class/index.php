40589f6cbb25fbe1d7b93c1ec495399d707b6ed6
syedhammadali1 <hammadali407292@gmail.com>
2024-01-21 20:45:30 +0500

    php first lecture

---
+++ PHP/First Class/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <?php 
+    // echo 'php script';    
+    // print 'print php script';    
+    // echo 'php script';  
+    /* This is a
+    multi-line comment */  
+
+    $newVariable = 'warisha';
+
+    // echo $newVariable;
+
+    // A variable name must start with a letter or the underscore character
+    $_sister = 'mehreen';
+
+    // A variable name cannot start with a number
+    // Variable names are case-sensitive 
+    // ($age and $AGE are two different variables)
+
+    echo $_sister;
+
+
+    //  PHP supports the following data types:
+
+    // String 'hammad'
+    // Integer (number 4,5,6)
+    // Float (floating point numbers - also called double) 4.5,5.6
+    // Boolean true,false
+    // Array array("Volvo","BMW","Toyota");
+    // Object
+    // NULL
+    // Resource
+    ?>
+</body>
+</html>
