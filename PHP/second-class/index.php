daed0fd6cd0ce9f98bc73df7b7b477eb9e0c2b8b
syedhammadali1 <hammadali407292@gmail.com>
2024-01-29 00:22:35 +0500

    php first and second class

---
+++ PHP/second-class/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello world class 2</h1>
+    <?php 
+        $name = 'karachi is my city';
+    //    echo ' $name';
+    //    echo " $name";
+    // echo strlen($name);
+    // echo str_word_count($name);
+    // echo strpos('little boy','boy');
+    // echo strtoupper('light');
+    // echo strtolower('LiGht');
+    // echo str_replace('karachi','lahore',$name);
+    // echo strrev('karachi');
+    // echo trim($name);
+
+    // var_dump( explode('is',$name));
+
+    $firstName = 'Arsalan';
+    $lastName = 'Ali';
+    
+    $fullName = $firstName . $lastName;
+
+    // echo $fullName;
+
+    // echo substr($firstName,2,3)
+
+    // 5 integer
+    // 5.5 float
+    // '6' number strings
+
+
+    
+
+    ?>
+</body>
+</html>
